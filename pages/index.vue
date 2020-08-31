e7233fe7cac9257a2bd49d91cf04644a11df0ea4
Martin Makarsky <martinmakarsky@gmail.com>
2020-08-31 15:25:55 +0200

    Get beer items using asyncData().

--- pages/index.vue
+++ pages/index.vue
@@ -18,39 +18,29 @@ export default {
     BeerListing
   },
 
-  data () {
-    return {
-      beerItems: []
-    }
-  },
+  async asyncData  () {
+    const deliveryClient = new DeliveryClient({
+      projectId: 'a8bbf063-f609-01e7-4870-223458b9b69d'
+    })
 
-  mounted () {
-    this.getBeerItems()
-  },
-
-  methods: {
-    getBeerItems () {
-      const deliveryClient = new DeliveryClient({
-        projectId: 'a8bbf063-f609-01e7-4870-223458b9b69d'
-      })
-
-      deliveryClient
-        .items()
-        .type('beer')
-        .toPromise()
-        .then((response) => {
-          this.beerItems = response.items.map((item) => {
-            return {
-              id: item.system.id,
-              title: item.title.value,
-              description: item.description.value,
-              sourceUrl: item.source.value,
-              imageSrc: item.image.value[0].url,
-              imageCaption: item.image.value[0].description
-            }
-          })
+    return deliveryClient
+      .items()
+      .type('beer')
+      .toPromise()
+      .then((response) => {
+        const beerItems = response.items.map((item) => {
+          return {
+            id: item.system.id,
+            title: item.title.value,
+            description: item.description.value,
+            sourceUrl: item.source.value,
+            imageSrc: item.image.value[0].url,
+            imageCaption: item.image.value[0].description
+          }
         })
-    }
+        return { beerItems }
+      })
   }
+
 }
 </script>
